2c1b4091b7533ecef5a1b3f2c5d7d47c60a152f7
TomashGombosh <blackjneco@gmail.com>
2022-07-26 00:42:17 +0300

    [API] Add image as Admin. View image as not authorized user

--- src/main/java/com/afj/solution/buyitapp/service/product/ProductService.java
+++ src/main/java/com/afj/solution/buyitapp/service/product/ProductService.java
@@ -1,7 +1,11 @@
 package com.afj.solution.buyitapp.service.product;
 
+import java.io.IOException;
+import java.util.UUID;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.afj.solution.buyitapp.model.Product;
 import com.afj.solution.buyitapp.payload.request.CreateProductRequest;
@@ -11,7 +15,14 @@ import com.afj.solution.buyitapp.payload.response.ProductResponse;
  * @author Tomash Gombosh
  */
 public interface ProductService {
+
     Page<ProductResponse> getProducts(Pageable pageable);
 
     Product save(CreateProductRequest createProductRequest);
+
+    ProductResponse addImageToProduct(UUID id, MultipartFile file) throws IOException;
+
+    byte[] getImageByProductId(UUID id);
+
+    Product findById(UUID id);
 }
