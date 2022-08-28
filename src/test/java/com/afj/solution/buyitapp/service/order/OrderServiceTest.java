14d979357228710df27a09a0e7aa0bf26188e5d9
TomashGombosh <blackjneco@gmail.com>
2022-08-28 08:20:16 +0300

    [UNIT] Test setup

--- src/test/java/com/afj/solution/buyitapp/service/order/OrderServiceTest.java
+++ src/test/java/com/afj/solution/buyitapp/service/order/OrderServiceTest.java
@@ -1,16 +1,9 @@
 package com.afj.solution.buyitapp.service.order;
 
-import java.util.UUID;
-
 import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import com.afj.solution.buyitapp.payload.request.CreateOrderRequest;
 
 /**
  * @author Tomash Gombosh
@@ -22,7 +15,6 @@ class OrderServiceTest implements WithAssertions {
     @Test
     @DisplayName("Create Order")
     void createOrder() {
-        final UUID userId = UUID.randomUUID();
-        final CreateOrderRequest createOrderRequest = new CreateOrderRequest();
+        assertThat(true).isTrue();
     }
 }
