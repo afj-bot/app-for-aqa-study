347c2db818a2f854df319c72c4f063c67dc6d168
TomashGombosh <blackjneco@gmail.com>
2022-08-29 19:15:41 +0300

    [INFRA] CORS policy

--- src/main/java/com/afj/solution/buyitapp/config/nonprod/WebMvcDevConfig.java
+++ src/main/java/com/afj/solution/buyitapp/config/nonprod/WebMvcDevConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Sort;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -28,6 +29,17 @@ import com.afj.solution.buyitapp.common.Response;
 @Configuration
 @Profile({"dev"})
 public class WebMvcDevConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedMethods("*")
+                .allowedOriginPatterns("https://*.afj-solution.com:[*]")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
 
     @Bean
     public Docket productApi() {
