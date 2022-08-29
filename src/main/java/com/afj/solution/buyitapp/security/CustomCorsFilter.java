0781a48cc6a016e34bc9679cbf1de58a48af64ba
TomashGombosh <blackjneco@gmail.com>
2022-08-29 19:05:17 +0300

    [INFRA] CORS policy

--- src/main/java/com/afj/solution/buyitapp/security/CustomCorsFilter.java
+++ src/main/java/com/afj/solution/buyitapp/security/CustomCorsFilter.java
@@ -1,6 +1,7 @@
 package com.afj.solution.buyitapp.security;
 
 import java.io.IOException;
+import java.util.UUID;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -18,11 +19,12 @@ import org.springframework.stereotype.Component;
  */
 @Slf4j
 @Component
-public class CorsFilter implements Filter {
+public class CustomCorsFilter implements Filter {
 
     @Override
     public void doFilter(final ServletRequest req, final ServletResponse res, final FilterChain chain)
             throws IOException, ServletException {
+        final String id = UUID.randomUUID().toString();
         final HttpServletResponse response = (HttpServletResponse) res;
         final HttpServletRequest request = (HttpServletRequest) req;
         response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
@@ -30,7 +32,9 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("RequestId", id);
         chain.doFilter(req, res);
+        log.info("Do CORS filter for the request id {}", id);
     }
 
     @Override
