643f1aed80381b4154ae43b3ec2d1e5e02ff4ffe
TomashGombosh <blackjneco@gmail.com>
2022-08-29 18:52:35 +0300

    [INFRA] CORS policy

---
+++ src/main/java/com/afj/solution/buyitapp/security/CorsFilter.java
@@ -0,0 +1,45 @@
+package com.afj.solution.buyitapp.security;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Tomash Gombosh
+ */
+@Slf4j
+@Component
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(final ServletRequest req, final ServletResponse res, final FilterChain chain)
+            throws IOException, ServletException {
+        final HttpServletResponse response = (HttpServletResponse) res;
+        final HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void init(final FilterConfig filterConfig) {
+        log.info("Init filter");
+    }
+
+    @Override
+    public void destroy() {
+        log.info("Destoy filter");
+    }
+}
